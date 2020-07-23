8b583f596109bb97e0d7b8ef773c6fff96d31d34
oclyke <oclyke@gmail.com>
2020-07-23 17:23:19 -0600

    support hardware serial
    provides a default UART object 'Serial' through which the console is routed
    allows additional UART objects on other pins

--- core-extend/HardwareSerial.h
+++ core-extend/HardwareSerial.h
@@ -20,10 +20,43 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-#ifndef _ARDUINO_MBED_BRIDGE_CORE_EXTEND_ARDUINOAPI_H_
-#define _ARDUINO_MBED_BRIDGE_CORE_EXTEND_ARDUINOAPI_H_
+#ifndef _ARDUINO_MBED_BRIDGE_CORE_EXTEND_HARDWARESERIAL_H_
+#define _ARDUINO_MBED_BRIDGE_CORE_EXTEND_HARDWARESERIAL_H_
 
-#include "core-extend/Common.h"
-#include "core-extend/HardwareSerial.h"
+#include "bridge/pins.h"
+#include "core-api/api/RingBuffer.h"
 
-#endif // _ARDUINO_MBED_BRIDGE_CORE_EXTEND_ARDUINOAPI_H_
+#define RX_BUF_LEN (256)
+
+class UART : public HardwareSerial, public mbed::UnbufferedSerial {
+private:
+    RingBufferN<RX_BUF_LEN> _rxbuf;
+
+protected:
+public:
+    UART(PinName tx, PinName rx, PinName rts = NC, PinName cts = NC);
+	UART(pin_size_t tx, pin_size_t rx, pin_size_t rts = variantPinCount, pin_size_t cts = variantPinCount);
+    UART( void );
+	~UART( void );
+
+    void rxISR( void );
+
+    void begin(unsigned long baudrate, uint16_t config);
+    void begin(unsigned long baudrate);
+    void end( void );
+    int available(void);
+    int peek(void);
+    int read(void);
+    void flush(void);
+    size_t write(uint8_t c);
+    size_t write(const uint8_t* buffer, size_t size);
+    using Print::write;
+    int printf(const char *format, ...);
+    operator bool(){
+        return true;
+    }
+};
+
+extern UART Serial;
+
+#endif // _ARDUINO_MBED_BRIDGE_CORE_EXTEND_HARDWARESERIAL_H_
